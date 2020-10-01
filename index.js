a5a0e8191d9d7e7d6cb3f1d3feb64717a8496e33
Elia <frodohack@gmail.com>
2020-10-01 13:12:47 +1000

    Clean

--- index.js
+++
@@ -1,9 +0,0 @@
-const YTSubtitles = require('./dist/index').default
-
-const id = YTSubtitles.extractIdFromUrl('https://www.youtube.com/watch?v=vG-QZOTc5_Q')
-
-YTSubtitles.getLanguagesList(id).then((resp) => {
-  resp[0].fetch().then(lang => {
-    console.log(lang)
-  })
-})
